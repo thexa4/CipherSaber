dc02a3807878905a82df6768f11fe02c43fd353a
Max Maton <thexa4@gmail.com>
2012-12-12 02:59:02 +0100

    Removed unrunnable unit tests and added application to test the cipher

---
+++ CSUtil/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CipherSaber;
+
+namespace CSUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arcfour generator = new Arcfour(Encoding.ASCII.GetBytes("Key"));
+            byte[] res = new byte[10];
+            generator.Read(res, 0, 10);
+            Console.WriteLine(BitConverter.ToString(res).Replace("-",""));
+
+            Console.WriteLine("EB9F7781B734CA72A719");
+
+            Console.ReadLine();
+        }
+    }
+}
